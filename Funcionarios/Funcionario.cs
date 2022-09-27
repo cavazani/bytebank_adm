8e4488faa0896fbed14c80740924d929d861ae6a
Gabriel Cavazani <gabriel_cavazani@hotmail.com>
2022-09-26 22:43:12 -0400

    Classe Funcionario

--- Funcionarios/Funcionario.cs
+++ Funcionarios/Funcionario.cs
@@ -5,6 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace bytebank_ADM.Funcionarios {
-    public class Funcionario {
+    public class Funcionario 
+    {
+        public string nome;
+        public string cpf;
+        public double salario;
+
+        public double getBonificacao() 
+        {
+            return salario * 0.1;   
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
-﻿namespace bytebank_ADM {
+﻿using bytebank_ADM.Funcionarios;
+namespace bytebank_ADM 
+{
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("ByteBank - ADM");
+
+            Funcionario pedro = new Funcionario();
+            pedro.nome = "pedro";
+            pedro.cpf = "12135656";
+            pedro.salario = 2000;
+
+            Console.WriteLine("Bonificacao: " + pedro.getBonificacao()); 
         }
     }
 }
